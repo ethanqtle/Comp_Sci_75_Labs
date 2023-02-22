a598430ca50a5d5a4dd20623962f72f51001ddd5
Nguyen Le <lenguyen_d@hotmail.com>
2023-02-22 14:36:18 -0800

    Add problem statements

--- CS076/ProgrammingLab/EthanLeLab03.cpp
+++ CS076/ProgrammingLab/EthanLeLab03.cpp
@@ -1,3 +1,26 @@
+//  A corporation has six divisions, each responsible for sales to different geographic locations.
+// 
+//  Design a DivSales class that keeps sales data for a division, with the following members: 
+// 
+//  • An array with four elements for holding four quarters of sales figures for the division.
+// 
+//  • A private static variable for holding the total corporate sales for all divisions for
+//    the entire year.
+// 
+//  • A member function that takes four arguments, each assumed to be the sales for a quarter.
+//    The value of the arguments should be copied into the array that holds the sales data.
+//    The total of the four arguments should be added to the static variable that holds the 
+//    total yearly corporate sales.
+// 
+//  • A function that takes an integer argument within the range of 0–3. The argument is to be 
+//    used as a subscript into the division quarterly sales array. The function should return
+//    the value of the array element with that subscript.
+// 
+// Write a program that creates an array of six DivSales objects. The program should ask the 
+// user to enter the sales for four quarters for each division. After the data are entered,
+// the program should display a table showing the division sales for each quarter. The
+// program should then display the total corporate sales for the year
+
 #include <iostream>
 using namespace std;
 
@@ -20,22 +43,19 @@ public:
         return totalCorporateSales;
     }
 };
-double DivSales::totalCorporateSales = 0;
+
+double DivSales::totalCorporateSales = 0.0;
 
 int main() {
     DivSales divisions[6];
-    double q1, q2, q3, q4;
+    double qSale[4];
     for (int i = 0; i < 6; i++) {
         cout << "Enter sales for division " << i+1 << ":\n";
-        cout << "Quarter 1: ";
-        cin >> q1;
-        cout << "Quarter 2: ";
-        cin >> q2;
-        cout << "Quarter 3: ";
-        cin >> q3;
-        cout << "Quarter 4: ";
-        cin >> q4;
-        divisions[i].setSales(q1, q2, q3, q4);
+        for(int j=0;j<4;j++){
+            cout << "Quarter " << i << ": ";
+            cin >> qSale[j];
+        }
+        divisions[i].setSales(qSale[0], qSale[1], qSale[2], qSale[3]);
     }
     cout << "\nDivision sales:\n";
     cout << "         Q1        Q2        Q3        Q4\n";
